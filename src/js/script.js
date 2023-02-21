3e4f1948ef24ad3916aff176b7c09c8a75a74464
Andrew <Papish_krolser@mail.ru>
2023-02-21 23:27:06 +0300

    next adaptive block direction

--- src/js/script.js
+++ src/js/script.js
@@ -70,3 +70,22 @@ $(document).ready(function(){
 }else{
     body.classList.add('mouse');
 }
+
+
+$(function(){
+  $(".direction__arrow").click(function(){ 
+      $(".direction__item").toggleClass('open');
+    });
+  });
+
+
+/* 
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    });
+}; */
